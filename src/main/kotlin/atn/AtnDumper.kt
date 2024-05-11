d12d7c74ac7e1c45a0d4c4714f04a43a1f220109
Ivan Kochurkin <kvanttt@gmail.com>
2024-05-11 16:12:53 +0200

    `AtnDumper`: render transition number if state has multiple transitions
    It matters because the order of rules and alternatives should be preserved

--- src/main/kotlin/atn/AtnDumper.kt
+++ src/main/kotlin/atn/AtnDumper.kt
@@ -12,7 +12,7 @@ import kotlin.text.any
 class AtnDumper(private val printTransLocation: Boolean = false, private val lineBreak: String = "\n") {
     companion object {
         private const val INDENT = "  "
-        private val enquoteChars = setOf('(', ')', '{', '}', ',')
+        private val enquoteChars = setOf('(', ')', '{', '}', '[', ']', ',', ' ')
     }
 
     private val visitedStates: MutableSet<State> = mutableSetOf()
@@ -61,27 +61,30 @@ class AtnDumper(private val printTransLocation: Boolean = false, private val lin
         if (!visitedStates.add(state)) return
 
         val stateName = state.getName()
-        state.outTransitions.forEach {
+        val multipleTransitions = state.outTransitions.size > 1
+        state.outTransitions.forEachIndexed { index, transition ->
             append(INDENT)
             append(stateName)
             append(" -> ")
-            append(it.target.getName())
+            append(transition.target.getName())
             append(" [label=")
-            val transitionLabel = when (it) {
-                is EpsilonTransition -> "ε"
-                is SetTransition -> it.set.dumpSet()
-                is RuleTransition -> "rule(${it.rule.ruleNode.idToken.value!!})"
-                is EndTransition -> "end(${it.rule.ruleNode.idToken.value!!})"
-                else -> TODO("Not implemented transition type: $it")
-            }
+            val transitionLabel =
+                when (transition) {
+                    is EpsilonTransition -> "ε"
+                    is SetTransition -> transition.set.dumpSet()
+                    is RuleTransition -> "rule(${transition.rule.ruleNode.idToken.value!!})"
+                    is EndTransition -> "end(${transition.rule.ruleNode.idToken.value!!})"
+                    else -> TODO("Not implemented transition type: $transition")
+                } +
+                (if (multipleTransitions) " [$index]" else "")
             append(transitionLabel.escapeAndEnquoteIfNeeded())
-            if (it is EndTransition) {
+            if (transition is EndTransition) {
                 append(", style=dotted")
             }
             append("]")
             append(lineBreak)
 
-            dump(it.target)
+            dump(transition.target)
         }
     }
 
